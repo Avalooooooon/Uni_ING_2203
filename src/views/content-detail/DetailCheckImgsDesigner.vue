09f54088959e67b386c927587bb249b5e28298f9
Avalon <2842240341@qq.com>
2022-04-07 17:32:09 +0800

    app系列详情显示数据

--- src/views/content-detail/DetailCheckImgsDesigner.vue
+++ src/views/content-detail/DetailCheckImgsDesigner.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- ”app内容管理“——设计师——设计师详情-->
   <div class="content-wrapper">
     <div class="topbar-wrapper">
       <div class="back" @click="toback">
@@ -8,107 +9,166 @@
     </div>
     <div class="content">
       <div class="content-left">
-        <el-form ref="numberValidateForm" :model="numberValidateForm" label-width="100px" style="margin-top: 30px" class="demo-dynamic">
+        <el-form
+          ref="numberValidateForm"
+          :model="numberValidateForm"
+          label-width="100px"
+          style="margin-top: 30px"
+          class="demo-dynamic"
+        >
           <el-form-item
-            label="名称"
+            label="姓名"
             prop="name"
-            :rules="[
-              { required: true, message: '名称不能为空'}
-            ]"
+            :rules="[{ required: true, message: '名称不能为空' }]"
           >
-            <el-input v-model.number="numberValidateForm.name" placeholder="请输入名称" type="age" style="width: 150px" ocomplete="off" />
+            <el-input
+              v-model.number="numberValidateForm.name"
+              placeholder="请输入名称"
+              type="age"
+              style="width: 250px"
+              ocomplete="off"
+            />
           </el-form-item>
-          <el-form-item
-            label="封面图"
-            prop="image"
-            required
-          >
+
+          <el-form-item label="照片" prop="image" required>
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
             <el-upload
+              v-else
               class="avatar-uploader"
               list-type="picture-card"
-              action="/v3upload/admin_wx_wallpaper"
+              action="/v3upload/admin_wx_xxx"
               :show-file-list="false"
               :on-success="handleAvatarSuccess"
               :before-upload="beforeAvatarUpload"
               :http-request="uploadFile"
             >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon" />
+              <i class="el-icon-plus avatar-uploader-icon" />
             </el-upload>
-          </el-form-item>
-          <el-form-item
-            label="专业"
-            prop="major"
-          >
-            <el-input v-model.number="numberValidateForm.major" placeholder="请输入专业名称" type="age" style="width: 150px" ocomplete="off" />
-          </el-form-item>
-          <el-form-item
-            label="职位"
-            prop="major"
-          >
-            <el-input v-model.number="numberValidateForm.position" placeholder="请输入职位名称" type="age" style="width: 150px" ocomplete="off" />
-          </el-form-item>
-          <el-form-item
-            label="国家"
-            prop="country"
-          >
-            <el-input v-model.number="numberValidateForm.country" placeholder="请输入国家名称" type="age" style="width: 150px" ocomplete="off" />
-          </el-form-item>
-        </el-form></div>
-      <div class="content-right">
-        <!--        <el-form-item prop="content" style="margin-bottom: 30px;">-->
-        <Tinymce ref="editor" v-model="numberValidateForm.content" style="margin-top: 30px" :width="600" :height="800" />
-        <!--        </el-form-item>-->
-        <!--        </el-form>-->
-      </div>
 
+
+          </el-form-item>
+
+          <el-form-item label="专业" prop="major">
+            <el-input
+              v-model.number="numberValidateForm.major"
+              placeholder="请输入专业名称"
+              type="age"
+              ocomplete="off"
+            />
+          </el-form-item>
+          
+          <el-form-item label="任职" prop="position">
+            <el-input
+              v-model.number="numberValidateForm.position"
+              placeholder="请输入职位名称"
+              type="age"
+              ocomplete="off"
+            />
+          </el-form-item>
+
+          <el-form-item label="所在国家" prop="country">
+            <el-input
+              v-model.number="numberValidateForm.country"
+              placeholder="请输入国家名称"
+              type="age"
+              ocomplete="off"
+            />
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="content-right">
+        <Tinymce
+          ref="editor"
+          v-model="numberValidateForm.content"
+          style="margin-top: 0px"
+          :width="590"
+          :height="700"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getToken } from '@/utils/auth'
-import Tinymce from '@/components/Tinymce'
+import {
+  fetchDesignerListDetail,
+  designerListUpload,
+  delDesignerListDetail,
+} from "@/api/appdesigner";
+import { getToken } from "@/utils/auth";
+import Tinymce from "@/components/Tinymce";
 // import axios from 'axios'
 
 export default {
-  name: 'DetailCheckImgsDesigner',
+  name: "DetailCheckImgsDesigner",
   components: { Tinymce },
-  props: ['detailid', 'detailname'],
+  props: ["detailid", "detailname"],
   data() {
     return {
+      url: "https://www.bizspace.cn",
+      imageUrl: "",
+      numberValidateForm: {
+        name: "",
+        content: "",
+        image: "",
+        major: "",
+        position: "",
+        country: "",
+      },
 
       // 发送给后端的数据
       designerParams: {
-        bizid: 'uniwarm',
+        bizid: "uniwarm",
         token: getToken(),
-        se_id: this.detailid,
-        page: 1
+        listid: 8,
+        itemid: this.detailid,
       },
-      url: 'https://www.bizspace.cn',
-      imageUrl: '',
-      numberValidateForm: {
-        name: '',
-        content: '',
-        major: '',
-        position: '',
-        country: ''
-      }
-    }
+      designerParams1: {
+        bizid: "uniwarm",
+        token: getToken(),
+        s_id: this.detailid,
+      },
+      designerParams2: {
+        bizid: "uniwarm",
+        token: getToken(),
+        img_id: "",
+      },
+
+      // 后端传来的数据
+      designerData: [],
+    };
   },
 
   created() {
+    this.getDesignerList();
   },
 
   methods: {
     toback() {
-      this.$router.go(-1)
+      this.$router.go(-1);
+    },
+
+    getDesignerList() {
+      fetchDesignerListDetail(this.designerParams)
+        .then((response) => {
+          console.log(response.detail);
+          this.designerData = response.detail;
+          this.numberValidateForm.name = this.designerData.name;
+          this.imageUrl = this.url + this.designerData.image;
+          this.numberValidateForm.major = this.designerData.major;
+          this.numberValidateForm.position = this.designerData.position;
+          this.numberValidateForm.country = this.designerData.country;
+          this.numberValidateForm.content = this.designerData.content;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
 
     uploadFile(file) {
-      console.log(file)
-      var formData = new FormData()
-      formData.append('headimg', file.file)
+      console.log(file);
+      var formData = new FormData();
+      formData.append("headimg", file.file);
       // morningListUpload(this.morningParams1, formData).then((res) => {
       //   console.log(res)
       //   this.imageUrl = this.url + res.image
@@ -124,22 +184,22 @@ export default {
       // })
     },
     handleAvatarSuccess(res, file) {
-      console.log(file)
+      console.log(file);
     },
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg'
-      const isLt2M = file.size / 1024 / 1024 < 2
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!')
+        this.$message.error("上传头像图片只能是 JPG 格式!");
       }
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
+        this.$message.error("上传头像图片大小不能超过 2MB!");
       }
-      return isJPG && isLt2M
-    }
-  }
-}
+      return isJPG && isLt2M;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -155,22 +215,6 @@ export default {
   height: calc(100vh - 50px);
   overflow-y: scroll;
   margin: 0 1%;
-}
-.content{
-  width: 100%;
-  height: calc(100vh - 100px);
-  //border: 1px solid black;
-  display: flex;
-  .content-left{
-    width: 30%;
-    height: 100%;
-    //border: 1px solid red;
-  }
-  .content-right{
-    width: 70%;
-    height: 100%;
-    //border: 1px solid blue;
-  }
 }
 // 顶部区域
 .topbar-wrapper {
@@ -249,68 +293,53 @@ export default {
     }
   }
 }
-
-// 主要内容区域（多个）
-.images-wrapper {
+// 主要内容显示区域
+.content {
   width: 100%;
-  height: 80%;
+  height: calc(100vh - 100px);
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  align-content: flex-start;
-  // 单图模块
-  .singleimage {
-    position: relative;
-    width: 19.6%; // (100-1*2)/5，根组件的左右margin为1%
+  
+  // 左侧列表
+  .content-left {
+    width: 40%;
     height: 100%;
-    padding: 1% 5px 0 5px;
-
-    .block {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-content: flex-start;
-      width: 100%;
-      height: 100%;
-      // 模块主图
-      .appimg {
-        width: 100%;
-        height: 95%;
-      }
-      // 删除状态下的删除栏
-      .delete-img {
+    // 列表输入框
+    ::v-deep {
+      .el-form {
         display: flex;
         flex-direction: column;
-        justify-content: center;
-
-        width: 100%;
-        position: absolute;
-        bottom: 0px;
-        height: 30px;
-        line-height: 30px;
-        margin: 0;
-        background: rgba(59, 60, 61, 0.5);
-        z-index: 999;
-        cursor: pointer;
-        text-align: center;
-        i {
-          display: block;
-          font-size: 20px;
-          color: white;
+        .el-input {
+          width: 250px;
         }
       }
     }
+    // 后台拿到的/待上传主图
+    .avatar{
+      width:250px;
+    }
   }
-}
-.footer{
-  width: 100%;
-  //border: 1px solid black;
-  text-align: center;
-  .el-pagination{
-    margin-bottom: 40px;
-    margin-top: 40px;
+
+  // 右侧编辑器
+  .content-right {
+    width: 60%;
+    height: 100%;
+    // 需要隐藏的部分
+    ::v-deep {
+      // 顶部栏除了upload按钮的其他按钮
+      .mce-container-body.mce-flow-layout{
+        // visibility:hidden;
+        width:100%;
+      }
+      // 顶部栏下面的工具栏
+      .mce-toolbar-grp.mce-container.mce-panel.mce-last {
+        // display: none;
+        width:100%;
+      }
+      .mce-container.mce-flow-layout-item.mce-first.mce-last.mce-btn-group{
+        width:100%;
+
+      }
+    }
   }
 }
 </style>
